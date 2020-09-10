ad3c2bd5ea590f87643cf6f52ffed880ce4b768e
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-10 13:09:20 +0900

    [finish]lesson25

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+  def comment_params
+    pramas.require(:comment).permit(:body)
+  end
 end
